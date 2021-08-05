cf9cc757a68954fe54f7595a1be3132d6355e80a
NataliaWoodson <74794694+NataliaWoodson@users.noreply.github.com>
2021-08-05 17:04:45 -0400

    added search form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from "./logo.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" width="300" />" 
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" width="300" />
+        </header>
+        <Dictionary />
+        <footer className=".App-footer">
+          <small>Coded by Natalia Woodson</small> 
+        </footer>
+      </div>
     </div>
   );
 }
